54647973e09d0d62d1bad96cfcd0d3668b0481ce
Dylan Miao <dtm32@pitt.edu>
2018-09-07 21:37:34 -0400

    started game files

---
+++ Game/Game/Game/Tile.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public class Tile
+    {
+        private Vector2   position;
+        private Rectangle tileRect;
+        private Texture2D tileTexture;
+        private bool      hasUnit = false;
+        private bool      hasTerrain = false;
+         
+
+        public Tile(int x, int y)
+        {
+            position.X = x;
+            position.Y = y;
+            tileRect = new Rectangle(x, y, 100, 100);
+        }
+
+        public void SetTexture(Texture2D texture)
+        {
+            tileTexture = texture;
+        }
+
+        public void AddUnit(Rectangle unit, Texture2D texture)
+        {
+            hasUnit = true;
+        }
+
+        public Rectangle GetRectangle()
+        {
+            return tileRect;
+        }
+
+        public Texture2D GetTexture()
+        {
+            return tileTexture;
+        }
+    }
+}
